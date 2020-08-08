09d399eafffbdca8b064923e05220c4a9b77d5d0
HillelMed <hillel95med@gmail.com>
2020-08-08 14:34:18 +0300

    add

--- src/main/java/com/faceforward/utils/service/ExcelService.java
+++ src/main/java/com/faceforward/utils/service/ExcelService.java
@@ -1,10 +1,12 @@
 package com.faceforward.utils.service;
 
+import com.faceforward.utils.exception.ApplicationException;
+
 import java.util.List;
 
 public interface ExcelService {
 
-    List<String> getValuesFromColumn(String columnName,String pathXls) throws Exception;
+    List<String> getValuesFromColumn(String columnName,String pathXls) throws ApplicationException;
     
     void printAllExcel(String path);
     
